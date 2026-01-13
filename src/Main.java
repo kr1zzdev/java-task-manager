e38f459702c7b62a9093099b7b9f5be092017740
Chrystopher Silvacky <chrystophersilvackyd@gmail.com>
2026-01-12 22:46:06 -0300

    Integrate TaskService into Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import model.Task;
+import service.TaskService;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Task Manager started.");
+
+        TaskService service = new TaskService();
+
+        Task t1 = new Task(1, "Study Java");
+        Task t2 = new Task(2, "Build portfolio project");
+
+        service.addTask(t1);
+        service.addTask(t2);
+
+        System.out.println("Tasks:");
+        service.listTasks().forEach(t ->
+                System.out.println(t.getId() + " - " + t.getTitle())
+        );
     }
 
 }
